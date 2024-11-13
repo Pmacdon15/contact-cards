58d6ceff89de3c291f35b42a09a27d2a17455783
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-11-13 12:10:06 -0700

    change colors softer

--- src/components/ui/save-edit-button/EditButton.tsx
+++ src/components/ui/save-edit-button/EditButton.tsx
@@ -2,7 +2,7 @@ export default function EditButton({ isEditing, setIsEditing }: { isEditing: boo
   return (
     <button
       onClick={() => setIsEditing(!isEditing)}
-      className={`p-2 items-center w-3/6  md:w-1/6 m-2 rounded-md shadow-lg text-black ${isEditing ? 'bg-red-600' : 'bg-gray-400 '}`}        >
+      className={`p-2 items-center w-3/6  md:w-1/6 m-2 rounded-md shadow-lg text-black ${isEditing ? 'bg-red-400' : 'bg-gray-400 '}`}        >
       {isEditing ? "Cancel" : "Edit"}
     </button>
   );
